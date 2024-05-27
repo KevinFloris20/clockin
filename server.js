933dd52d64c2387abcf9c5f56994dba044b176af
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2024-05-27 14:56:15 -0400

    fixed a bug but got a new one

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const clockInRoutes = require('./server/routes');
+const route = require('./server/routes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api', clockInRoutes);
+app.use('/api', route);
 
 
 app.listen(PORT, () => {
--- server/handlers/clockInHandler.js
+++ server/handlers/clockInHandler.js
@@ -2,7 +2,12 @@ const { validateUTCTimestamp } = require( '../apps/validation.js');
 exports.handleClockIn = (req, res) => {
     const { clockTime } = req.body;
 
+    console.log('clockTime:', clockTime);
+
     let { isvalid, msg } = validateUTCTimestamp(clockTime);
+
+    console.log('isvalid:', isvalid);
+    console.log('msg:', msg);
     
     if (!isvalid) {
         return res.status(400).json({ message: msg });
